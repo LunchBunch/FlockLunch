67398a776c7aa327d789689ffceeb235d41f721a
ArianaRodriguez <ariana.chivita@gmail.com>
2017-02-11 19:53:49 -0800

    Adding config file

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+	appId: '8a6f145c-dd6d-4d3c-a476-1741fef71432',
+    appSecret: '60bd861d-062e-4499-9848-c114faea5bb1',
+    botUserId: 'u:Bssndzwnjwwcjzec',
+    botToken: '3e21e595-d144-449c-a14e-b0600290f78d'
+}
