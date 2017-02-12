67be56a85588e3678ffd92ee44da405f63e8d1c7
ArianaRodriguez <ariana.chivita@gmail.com>
2017-02-12 13:16:29 -0800

    yelp api

--- index.js
+++ index.js
@@ -4,6 +4,13 @@ var express = require('express');
 var Mustache = require('mustache');
 var bodyParser = require('body-parser');
 var fs = require('fs');
+var Yelp = require('yelp');
+var yelp = new Yelp({
+  consumer_key: config.consumerKeyYelp,
+  consumer_secret: config.consumerSecretYelp,
+  token: config.tokenYelp,
+  token_secret: config.tokenSecretYelp
+});
 
 flock.appId = config.appId;
 flock.appSecret = config.appSecret;
@@ -22,9 +29,10 @@ app.listen(8080, function() {
 });
 
 flock.events.on('app.install', function(event, callback) {
-  console.log(event.userId);
-  console.log(event.token);
+  console.log('userId: ' + event.userId);
+  console.log('token: ' + event.token);
   console.log('app.install event');
+  store.saveToken(event.userId, event.token);
   callback();
 });
 
@@ -37,6 +45,7 @@ flock.events.on('', function() {
 });
 
 var listTemplate = fs.readFileSync('options.mustache.html', 'utf8');
+var first5Restaurants = [];
 app.get('/options', function (req, res) {
   console.log('GET /options');
   var event = JSON.parse(req.query.flockEvent);
@@ -48,11 +57,35 @@ app.get('/options', function (req, res) {
     { 'option': 'Greek' }
   ];
   res.set('Content-Type', 'text/html');
-  var body = Mustache.render(listTemplate, { restaurants: fakeRestaurants });
+  
+  var body = Mustache.render(listTemplate, { restaurants: fakeRestaurants, resultSearch: first5Restaurants });
   res.send(body);
 });
-
 app.post('/options', function (req, res) {
+  // storage.saveToken(userIdw, tokenIdw, req.params.choice );
   console.log('POST /options choice: ' + req.params.choice + ' --');
   console.log('POST /options choice: ' + req.body.choice + ' --');
+
+  // TODO add a process to calculate which restaurants are winners.
+  
+
+  yelp.search({
+    term: 'food ' + req.body.choice,
+    location: 'San Francisco'
+  }).then(function (data) {    
+    var fakeRestaurants = [
+      { 'option': 'Italian' },
+      { 'option': 'French' },
+      { 'option': 'American' },
+      { 'option': 'Brazilian' },
+      { 'option': 'Greek' }
+    ];
+    first5Restaurants = data.businesses;
+    console.log(data.businesses[0]);
+    res.set('Content-Type', 'text/html');
+    var body = Mustache.render(listTemplate, { restaurants: fakeRestaurants, resultSearch: first5Restaurants });
+    res.send(body);
+  }).catch(function (err) {
+    console.error(err);
+  });
 });
